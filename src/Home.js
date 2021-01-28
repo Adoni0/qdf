d080b2657e9cd7d78c11efb53fc54b3cba6dfdda
Adam SOudani <asoudani@email.com>
2021-01-27 19:31:39 -0800

    carousel setup layout

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Jumbotron } from 'react-bootstrap'
+import Pony from './Pony'
 
 const Home = () => {
     return (
-        <Jumbotron>
-            <h1>Quarantine Date Choices</h1>
-        </Jumbotron>
+        <>
+            <Jumbotron style={{ paddingBottom: 20 }}>
+                <h1>Quarantine Date Choices</h1>
+                <p>Choose One!</p>
+            </Jumbotron>
+            <Pony />
+        </>
     )
 }
 
--- src/Pony.js
+++ src/Pony.js
@@ -1,45 +1,31 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap'
+import { Carousel, Button } from 'react-bootstrap'
+import dateIdeas from './dateIdeas.json'
 
 const Pony = () => {
     return (
-        <Carousel>
-            <Carousel.Item>
+        <div>
+        <Carousel style={{ height: '400px', borderTop: 'inset', marginLeft: '50px', marginRight: '50px' }}>
+            {
+            dateIdeas.map(idea => (
+                
+                <Carousel.Item key={idea.id} style={{ height: '400px' }}>
                 <img
                     className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=373940"
-                    alt="First slide"
+                    src={idea.img}
+                    alt=""
+                   style={{ height: '400px' }}
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <Carousel.Caption style={{ marginBottom: 50 }}>
+                    <h3>{idea.date}</h3>
+                    <Button variant="info">Select</Button>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            
+            ))
+            }
         </Carousel>
+        </div>
     )
 }
 
