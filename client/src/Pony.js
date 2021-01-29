94a906c3495b569745851f963c5d33dc3d5cc314
Adam SOudani <asoudani@email.com>
2021-01-29 13:08:01 -0800

    alert fixed/ connect to heroku hopefully

--- client/src/Pony.js
+++ client/src/Pony.js
@@ -1,64 +1,68 @@
 import React, { useState } from 'react'
-import { Carousel, Button } from 'react-bootstrap'
+import { Carousel, Button, Alert } from 'react-bootstrap'
 import dateIdeas from './dateIdeas.json'
 import axios from 'axios'
-import SuccessAlert from './SuccessAlert'
 
 const Pony = () => {
 
-    const [isAlertShown, setIsAlertShown] = useState(false);
+    const [show, setShow] = useState(false);
 
     const onSelect = async (id) => {
         var selected = await dateIdeas.filter(idea => idea.id === id);
-        // console.log(selected[0].date)
         // axios.post('/date', { date: selected[0].date })
         axios({
             method: 'post',
             url: '/date',
             data: {
                 date: selected[0].date
-            }, 
+            },
             headers: {
                 "Content-Type": "application/json"
             }
         })
-        .then(response => showAlert())
-        .catch(err => console.log(err))
+            .then(response => showAlert())
+            .catch(err => console.log(err))
     }
 
     const showAlert = () => {
-        setIsAlertShown(true);
+        setShow(true);
         setTimeout(() => {
-            setIsAlertShown(false)
-        }, 4000)
+            setShow(false)
+        }, 5000)
     }
 
     return (
         <div>
-            {isAlertShown ? <SuccessAlert /> : null}
-        <Carousel style={{ height: '400px', borderTop: 'inset', marginLeft: '50px', marginRight: '50px' }}>
             {
-            dateIdeas.map(idea => (
-                
-                <Carousel.Item key={idea.id} style={{ height: '400px' }}>
-                <img
-                    className="d-block w-100"
-                    src={idea.img}
-                    alt=""
-                   style={{ height: '400px' }}
-                />
-                <Carousel.Caption style={{ marginBottom: 50 }}>
-                    <h3>{idea.date}</h3>
-                    <Button 
-                    onClick={() => onSelect(idea.id)} 
-                    variant="info"
-                    >Select</Button>
-                </Carousel.Caption>
-            </Carousel.Item>
-            
-            ))
+            show ? <Alert
+                variant="success" onClose={() => setShow(false)} dismissible>
+                <Alert.Heading>Selection Confirmed!</Alert.Heading>
+                <p>Adam has recieved a message with your selection and will get back to you soon.</p>
+            </Alert> : null
             }
-        </Carousel>
+            <Carousel style={{ height: '400px', borderTop: 'inset', marginLeft: '50px', marginRight: '50px' }}>
+                {
+                    dateIdeas.map(idea => (
+
+                        <Carousel.Item key={idea.id} style={{ height: '400px' }}>
+                            <img
+                                className="d-block w-100"
+                                src={idea.img}
+                                alt=""
+                                style={{ height: '400px' }}
+                            />
+                            <Carousel.Caption style={{ marginBottom: 50 }}>
+                                <h3>{idea.date}</h3>
+                                <Button
+                                    onClick={() => onSelect(idea.id)}
+                                    variant="info"
+                                >Select</Button>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+
+                    ))
+                }
+            </Carousel>
         </div>
     )
 }
--- server.js
+++ server.js
@@ -17,23 +17,27 @@ if (process.env.NODE_ENV === "production") {
 
 
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-// var client = new twilio(process.env.TWILIO_ACCOUNT_ID, process.env.TWILIO_AUTH_TOKEN);
+var client = new twilio(process.env.TWILIO_ACCOUNT_ID, process.env.TWILIO_AUTH_TOKEN);
 
 app.post('/date', (req, res) => {
     const { date } = req.body;
 
-    // client.messages.create({
-    //     to: '+18053772125',
-    //     from: '+13862208419',
-    //     body: `Rashelle chose: ${date}`
-    // });
+    client.messages.create({
+        to: '+18053772125',
+        from: '+13862208419',
+        body: `Rashelle chose: ${date}`
+    });
     console.log(date)
     res.status(200).send(date);
 })
+
+app.use(function (req, res, next) {
+    res.status(404).send('Unable to find the requested resource!');
+  });
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
